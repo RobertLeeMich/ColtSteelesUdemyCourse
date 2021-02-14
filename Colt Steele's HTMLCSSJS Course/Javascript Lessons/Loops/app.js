c898d88feedaab95e01b35576326ad30c0547247
Setari <guildwarsguy123@yahoo.com>
2021-02-14 17:43:53 -0500

    Update app.js
    example nested loop

--- Colt Steele's HTMLCSSJS Course/Javascript Lessons/Loops/app.js
+++ Colt Steele's HTMLCSSJS Course/Javascript Lessons/Loops/app.js
@@ -66,3 +66,10 @@ let animals = [
     //     ['Geoff', 'Juanita', 'Antonio', 'Kevin']
     //     ['Yuma', 'Sakura', 'Jack', 'Erika']
     // ]
+
+    // for (let i = 0; i <= 10; i++){
+    //     console.log(`i is ${i}`)
+    //     for (let j = 1; j <= 4; j++){
+    //         console.log (`       j is ${j}`)
+    //     }
+    // }
