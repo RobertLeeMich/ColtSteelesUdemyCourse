d8a95f89af10a656712e2f29b6217cabf5ff3c03
Setari <guildwarsguy123@yahoo.com>
2021-02-13 22:10:32 -0500

    commented pre-existing code
    commented pre-existing past lesson code out.

--- Colt Steele's HTMLCSSJS Course/Javascript Lessons/Loops/app.js
+++ Colt Steele's HTMLCSSJS Course/Javascript Lessons/Loops/app.js
@@ -2,9 +2,9 @@
 //     console.log(i);
 // };
 
-for (let i = 100; i >= 0; i -= 10) {
-    console.log(i);
-}
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
 
 let animals = [
     "Aardvark",
