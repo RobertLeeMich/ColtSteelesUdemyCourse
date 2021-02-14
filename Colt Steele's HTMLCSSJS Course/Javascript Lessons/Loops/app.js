1006f0f1cf5699dd9c1b4b81a2ed62f4a465d0f9
Setari <guildwarsguy123@yahoo.com>
2021-02-14 18:11:45 -0500

    Update app.js
    commented animals out, made loop to print list inside list.

--- Colt Steele's HTMLCSSJS Course/Javascript Lessons/Loops/app.js
+++ Colt Steele's HTMLCSSJS Course/Javascript Lessons/Loops/app.js
@@ -6,52 +6,52 @@
 //     console.log(i);
 // }
 
-let animals = [
-    "Aardvark",
-    "Abyssinian",
-    "Adelie Penguin",
-    "Affenpinscher",
-    "Afghan Hound",
-    "African Bush Elephant",
-    "African Civet",
-    "African Clawed Frog",
-    "African Forest Elephant",
-    "African Palm Civet",
-    "African Penguin",
-    "African Tree Toad",
-    "African Wild Dog",
-    "Ainu",
-    "Airedale Terrier",
-    "Akita",
-    "Alaskan Malamute",
-    "Albacore Tuna",
-    "Albatross",
-    "Aldabra Giant Tortoise",
-    "Alligator",
-    "Alligator Gar",
-    "Alpaca",
-    "Alpine Dachsbracke",
-    "Amazon River Dolphin (Pink Dolphin)",
-    "American Alsatian",
-    "American Bulldog",
-    "American Cocker Spaniel",
-    "American Coonhound",
-    "American Eskimo Dog",
-    "American Foxhound",
-    "American Pit Bull Terrier",
-    "American Staffordshire Terrier",
-    "American Water Spaniel",
-    "Amur Leopard",
-    "Anatolian Shepherd Dog",
-    "Anchovies",
-    "Angelfish",
-    "Ant",
-    "Anteater",
-    "Antelope",
-    "Appenzeller Dog",
-    "Arapaima",
-    "Arctic Fox",
-    ]
+// let animals = [
+//     "Aardvark",
+//     "Abyssinian",
+//     "Adelie Penguin",
+//     "Affenpinscher",
+//     "Afghan Hound",
+//     "African Bush Elephant",
+//     "African Civet",
+//     "African Clawed Frog",
+//     "African Forest Elephant",
+//     "African Palm Civet",
+//     "African Penguin",
+//     "African Tree Toad",
+//     "African Wild Dog",
+//     "Ainu",
+//     "Airedale Terrier",
+//     "Akita",
+//     "Alaskan Malamute",
+//     "Albacore Tuna",
+//     "Albatross",
+//     "Aldabra Giant Tortoise",
+//     "Alligator",
+//     "Alligator Gar",
+//     "Alpaca",
+//     "Alpine Dachsbracke",
+//     "Amazon River Dolphin (Pink Dolphin)",
+//     "American Alsatian",
+//     "American Bulldog",
+//     "American Cocker Spaniel",
+//     "American Coonhound",
+//     "American Eskimo Dog",
+//     "American Foxhound",
+//     "American Pit Bull Terrier",
+//     "American Staffordshire Terrier",
+//     "American Water Spaniel",
+//     "Amur Leopard",
+//     "Anatolian Shepherd Dog",
+//     "Anchovies",
+//     "Angelfish",
+//     "Ant",
+//     "Anteater",
+//     "Antelope",
+//     "Appenzeller Dog",
+//     "Arapaima",
+//     "Arctic Fox",
+//     ]
 
     // for (let i = 0; i <=animals.length; i++ ){
     //     console.log(i, animals[i]);
@@ -61,11 +61,11 @@ let animals = [
     //     console.log(i, animals[i]);
     // }
 
-    // const seatingChart= [
-    //     ['Kristen', 'Erik', 'Namita']
-    //     ['Geoff', 'Juanita', 'Antonio', 'Kevin']
-    //     ['Yuma', 'Sakura', 'Jack', 'Erika']
-    // ]
+    const seatingChart= [
+        ['Kristen', 'Erik', 'Namita'],
+        ['Geoff', 'Juanita', 'Antonio', 'Kevin'],
+        ['Yuma', 'Sakura', 'Jack', 'Erika']
+    ]
 
     // for (let i = 0; i <= 10; i++){
     //     console.log(`i is ${i}`)
@@ -73,3 +73,11 @@ let animals = [
     //         console.log (`       j is ${j}`)
     //     }
     // }
+
+    for (let i = 0; i < seatingChart.length; i++){
+        let row = seatingChart[i];
+        console.log(`ROW #${i + 1}`)
+        for (let j = 0; j < row.length; j++){
+            console.log(row[j]);
+        }
+    }
