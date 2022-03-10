a38f851b7537c27e72f1bfc86f99e7c3fccb9639
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-10 13:07:25 -0500

    Update app.js
    commented code out to save it and move onto next section

--- Colt Steele's HTMLCSSJS Course/ForOfLoops/ForOfObjectLoops/app.js
+++ Colt Steele's HTMLCSSJS Course/ForOfLoops/ForOfObjectLoops/app.js
@@ -1,13 +1,13 @@
-const gamescores = {
-    LostArk: 8,
-    EldenRing: 9,
-    MHR: 7,
-    Dyson: 6,
-    Hunt: 8,
-    Rock: 9,
-    Escape: 1,
-    Counter: 3
-}
+// const gamescores = {
+//     LostArk: 8,
+//     EldenRing: 9,
+//     MHR: 7,
+//     Dyson: 6,
+//     Hunt: 8,
+//     Rock: 9,
+//     Escape: 1,
+//     Counter: 3
+// }
 //for...OF does not let you iterate over key/value pairs such as these
 // for (let score of gamescores){
 //     console.log(score);
@@ -19,7 +19,14 @@ const gamescores = {
 //  }
 
 //to print the value of the key:
-for (let score in gamescores){
-    console.log(`${score} scored ${gamescores[score]}`)
-}
+// for (let score in gamescores){
+//     console.log(`${score} scored ${gamescores[score]}`)
+// }
 //what is happening here is we are iterating over the object and getting the key/value pairs inside, then printing the key (score), then getting the value of the gamescore key with the [score] which is the value of each key.
+
+//There is also a console command called Object.keys(pass in whatever variable is assigned to the object here) that will print the internals of the object (such as the key/value pairs here) as an array. IT PRINTS KEYS ONLY NOT VALUES IN KEY/VALUE PAIRS
+
+//Object.values will retrieve the values of the key/value pairs and print them in an array as well. This DOES NOT PRINT THE KEYS
+
+//Object.entries will give us a nested array of key/value pairs. THIS PRINTS BOTH THE KEYS AND THE VALUES
+
