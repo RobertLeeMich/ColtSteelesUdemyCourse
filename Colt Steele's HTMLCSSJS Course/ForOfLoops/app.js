e7b2ad1b9a0cd52a25fae612c25cf59f1900cf69
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-10 12:40:37 -0500

    Update app.js
    finished working with a for...of loop, commented out code for further notes

--- Colt Steele's HTMLCSSJS Course/ForOfLoops/app.js
+++ Colt Steele's HTMLCSSJS Course/ForOfLoops/app.js
@@ -1,4 +1,4 @@
-const games = ["Elden Ring", "Lost Ark", "Deep Rock Galactic", "American Truck Sim"];
+// const games = ["Elden Ring", "Lost Ark", "Deep Rock Galactic", "American Truck Sim"];
 
 
 /*A normal for loop*/
@@ -10,6 +10,30 @@ const games = ["Elden Ring", "Lost Ark", "Deep Rock Galactic", "American Truck S
     statement
 } */
 
-for (let item of games) {
-    console.log(item)
-}
+// for (let item of games) {
+//     console.log(item)
+// }
+
+/* another example */
+
+// const seats = [
+//     ["Seat1", "Seat2", "Seat3"],
+//     ["Seat4", "Seat5", "Seat6"],
+//     ["Seat7", "Seat8", "Seat9", "Seat10"]
+// ]
+
+/*
+for (let i = 0; i < seats.length; i++){
+    const row = seats[i];
+    for (let j = 0; j < row.length; j++){
+        console.log(row[j])
+    }
+}*/
+
+/* for (let row of seats){
+    console.log(row); //This prints each row of seats as an array on its own
+    for (let singularSeat of row) {
+        console.log(`${singularSeat}`)
+    } //This prints each seat on its own that is in the array
+} */
+
