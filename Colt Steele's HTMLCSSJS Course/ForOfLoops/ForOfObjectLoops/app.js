de3acd4d0bebd7fc432523c20953f5cabfb7ba25
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-10 13:02:47 -0500

    Update app.js
    wrote up how to correctly print values from an object such as this. Also fixed naming convention in the object.

--- Colt Steele's HTMLCSSJS Course/ForOfLoops/ForOfObjectLoops/app.js
+++ Colt Steele's HTMLCSSJS Course/ForOfLoops/ForOfObjectLoops/app.js
@@ -1,6 +1,6 @@
 const gamescores = {
     LostArk: 8,
-    Elden Ring: 9,
+    EldenRing: 9,
     MHR: 7,
     Dyson: 6,
     Hunt: 8,
@@ -8,3 +8,18 @@ const gamescores = {
     Escape: 1,
     Counter: 3
 }
+//for...OF does not let you iterate over key/value pairs such as these
+// for (let score of gamescores){
+//     console.log(score);
+// }
+
+//for...IN however will let you iterate over these objects. This will only print the KEY
+// for (let score in gamescores){
+//      console.log(score);
+//  }
+
+//to print the value of the key:
+for (let score in gamescores){
+    console.log(`${score} scored ${gamescores[score]}`)
+}
+//what is happening here is we are iterating over the object and getting the key/value pairs inside, then printing the key (score), then getting the value of the gamescore key with the [score] which is the value of each key.
