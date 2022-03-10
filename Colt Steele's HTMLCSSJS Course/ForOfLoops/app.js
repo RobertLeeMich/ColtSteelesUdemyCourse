720c54298905fa2736d9e2fdd6d0f7bdd3beb081
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-10 12:18:28 -0500

    Creation of the directory for for...of
    For...of loops

---
+++ Colt Steele's HTMLCSSJS Course/ForOfLoops/app.js
@@ -0,0 +1,8 @@
+const games = ["Elden Ring", "Lost Ark", "Deep Rock Galactic", "American Truck Sim"];
+
+
+/*A normal for loop*/
+/* for (let i = 0; i < games.length; i++) {
+    console.log(` Game: ${games[i]}`)
+} */
+
