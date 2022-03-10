6de711ba9d2baddeaf0a6b263f3b59fe752c2d81
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-10 12:46:36 -0500

    Update app.js
    I'm stupid it's "let" not "int" what am I doing

--- Colt Steele's HTMLCSSJS Course/ForOfLoops/app.js
+++ Colt Steele's HTMLCSSJS Course/ForOfLoops/app.js
@@ -45,6 +45,6 @@ for (let i = 0; i < seats.length; i++){
 const numbers = [1,2,3,4,5,6,7,8,9]; //DON'T CHANGE THIS LINE PLEASE!
 
 // WRITE YOUR LOOP BELOW THIS LINE:
-for (int item of numbers) {
+for (let item of numbers) {
     console.log(item * item);
 }
