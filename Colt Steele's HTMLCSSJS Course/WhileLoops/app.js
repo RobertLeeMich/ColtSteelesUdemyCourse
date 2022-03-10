c9523bc3896860b22f5862f0f6444fe6ec74f9e2
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-10 12:04:18 -0500

    Update app.js
    Added functionality for quitting the "game".

--- Colt Steele's HTMLCSSJS Course/WhileLoops/app.js
+++ Colt Steele's HTMLCSSJS Course/WhileLoops/app.js
@@ -7,12 +7,23 @@ const targetNum = Math.floor(Math.random() * maximum) + 1;
 console.log(targetNum);
 
 let guess = parseInt(prompt("Enter first guess."));
+let attempts = 1;
 
-while( guess !== targetNum){
+while(parseInt(guess) !== targetNum) {
+    if (guess === "q") break;
+    attempts++;
     if (guess > targetNum){
-       guess =  prompt("Too high, enter new guess")
+       guess =  prompt("Too high, enter new guess");
     }
     else {
-        guess = prompt("Too low, enter new guess.")
+        guess = prompt("Too low, enter new guess.");
     }
+}
+
+if (guess === "q"){
+    console.log("Okay, quitting.")
+}
+else {
+console.log("gg you win.")
+console.log(`You got it! It took you ${attempts} guesses!`);
 }
