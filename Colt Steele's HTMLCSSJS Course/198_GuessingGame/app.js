f5f63cb415c01b70b00c4ff3aa31f12b29071273
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-10 22:41:58 -0500

    removed a folder, made new folder for new project
    Deleted 198_GuessingGame, changed to WhileLoops folder since that was the folder I made for the lesson when I came back.
    
    Also began a basic todo list project.

--- Colt Steele's HTMLCSSJS Course/198_GuessingGame/app.js
+++
@@ -1,6 +0,0 @@
-let maximum = prompt("Enter the maximum number");
-
-Math.floor(Math.random() * maximum) +1
-
-console.log(targetNum)
-
---
+++ Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
@@ -0,0 +1,8 @@
+//Going to have a while loop
+//While the results are not equal to quit, just keep looping
+//storage of todos will be in an array
+let todos = []
+prompt(input);
+if (input === "new"){
+    todos.push(input);
+}
