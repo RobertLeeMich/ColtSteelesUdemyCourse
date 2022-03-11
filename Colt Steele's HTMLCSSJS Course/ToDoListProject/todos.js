a0eaa6e910c4edd73adb3a2f0547405cf24cc505
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-10 23:25:37 -0500

    Fixing load issues
    troubleshooting why the html doesn't load before the script runs. I need the script to wait until the page is loaded.

--- Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
+++ Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
@@ -1,8 +1,17 @@
+window.addEventListener('load', function () {
+    console.log("do some shit")
+  });
+
 //Going to have a while loop
 //While the results are not equal to quit, just keep looping
 //storage of todos will be in an array
 let todos = []
-prompt(input);
+let input = prompt("What would you like to do?")
+while (prompt !== "quit"){
 if (input === "new"){
-    todos.push(input);
+    let newInput = prompt("please add to the todo");
+    todos.push(newInput);
+    
+    }
+    break;
 }
