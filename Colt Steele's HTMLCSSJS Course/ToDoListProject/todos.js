8fce2f8c4401948633c5af902172ef45497c84ce
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-13 17:43:18 -0400

    Update todos.js
    created half of the lesson, got stuck on deleting a specific index via a prompt.

--- Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
+++ Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
@@ -1,17 +1,21 @@
-window.addEventListener('load', function () {
-    console.log("do some shit")
-  });
-
 //Going to have a while loop
 //While the results are not equal to quit, just keep looping
 //storage of todos will be in an array
 let todos = []
-let input = prompt("What would you like to do?")
-while (prompt !== "quit"){
-if (input === "new"){
-    let newInput = prompt("please add to the todo");
-    todos.push(newInput);
+const indexTest = todos.indexOf()
+window.setTimeout(function() {
+        let input = prompt("What would you like to do?")
+        while (prompt !== "quit"){
+            if (input === "new"){
+            let newInput = prompt("please add to the todo");
+            todos.push(newInput);
+            } else if (input === "list"){
+               console.log(todos[i]); 
+            } else if (input === "delete"){
+            
+            }
+        break;
+        }
     
-    }
-    break;
-}
+}, 100);
+
