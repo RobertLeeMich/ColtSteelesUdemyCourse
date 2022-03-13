530e8b10695e1cd0984fdbc2c9b77eadcce3bb48
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-13 17:52:28 -0400

    Update todos.js
    reverted to lesson code, can't figure out a way to do the splice on a specific item using the alert prompt.

--- Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
+++ Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
@@ -2,7 +2,6 @@
 //While the results are not equal to quit, just keep looping
 //storage of todos will be in an array
 let todos = []
-const indexTest = todos.indexOf()
 window.setTimeout(function() {
         let input = prompt("What would you like to do?")
         while (prompt !== "quit"){
@@ -10,9 +9,9 @@ window.setTimeout(function() {
             let newInput = prompt("please add to the todo");
             todos.push(newInput);
             } else if (input === "list"){
-               console.log(todos[i]); 
+               console.log(todos); 
             } else if (input === "delete"){
-            
+
             }
         break;
         }
