86103b446f804911b31a4427b399566f7fddcdf8
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-14 22:45:01 -0400

    Made new js file to compare code, figured out issue with original code.
    Figured it out, I had the !== statements set to prompt instead of input. Stoopy here.

--- Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
+++ Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
@@ -1,9 +1,9 @@
 /*Going to have a while loop
 While the results are not equal to quit, just keep looping
 storage of todos will be in an array */
-let todos = []
 let input = prompt("What would you like to do?")
-while (prompt !== "quit" && prompt !== "q"){
+let todos = ["cars", "trukks"]
+while (input !== "quit" && input !== "q"){
     if (input === "new"){
         let newInput = prompt("please add to the todo");
         todos.push(newInput);
--- Colt Steele's HTMLCSSJS Course/ToDoListProject/todos2.js
+++ Colt Steele's HTMLCSSJS Course/ToDoListProject/todos2.js
@@ -1,10 +1,20 @@
 let input = prompt("What would you like to do?");
-const todos =[];
+const todos =["Cars", "Trukks"];
 while (input !== "quit" && input !=="q") {
     if (input === "list"){
         for (item of todos){
-            console.log(todos[i]);
+            console.log(item);
         }
+    } else if (input === "new"){
+        const newTodo = prompt("Ok, what is the new todo?")
+        todos.push(newTodo);
+        console.log(`${newTodo} added to list.`)
+    } else if (input === "delete") {
+        const index = parseInt(prompt("Ok, enter the index to delete"))
+        if (!Number.isNaN(index)){
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else console.log("Unknown Index")
     }
     input = prompt("What would you like to do?");
 }
