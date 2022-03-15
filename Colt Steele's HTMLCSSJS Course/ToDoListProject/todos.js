28e54078c5788399d04dfb60ef33eafbf66748e8
Robert Michaud <guildwarsguy123@yahoo.com>
2022-03-14 22:33:13 -0400

    Update todos.js
    still trying to fix the loop, removed the waiting for the load page

--- Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
+++ Colt Steele's HTMLCSSJS Course/ToDoListProject/todos.js
@@ -3,19 +3,23 @@ While the results are not equal to quit, just keep looping
 storage of todos will be in an array */
 let todos = []
 let input = prompt("What would you like to do?")
-window.setTimeout(function() {
-        while (prompt !== "quit" && prompt !== "q"){
-            if (input === "new"){
-            let newInput = prompt("please add to the todo");
-            todos.push(newInput);
-            } else if (input === "list"){
-               console.log(todos); 
-            } else if (input === "delete"){
-            const index = prompt("Ok, enter an index to delete:")
-            todos.splice(index, 1);
+while (prompt !== "quit" && prompt !== "q"){
+    if (input === "new"){
+        let newInput = prompt("please add to the todo");
+        todos.push(newInput);
+        } else if (input === "list"){
+            console.log(todos); 
+        } else if (input === "delete"){
+        const index = parseInt( prompt("Ok, enter an index to delete:"));
+        if (!Number.isNaN(index)) {
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+            } else {
+            console.log ("Unknown Index, pls try again")
             }
-        break;
         }
+            
+        break;
+}
         input = prompt("What would you like to do?")
     
-}, 100);
