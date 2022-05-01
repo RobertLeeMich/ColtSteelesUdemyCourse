1724fc166736d18fb84a160c0223cc1e67ff47ea
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-01 18:44:11 -0400

    boilerplate, lesson
    Added boilerplate, lesson code

---
+++ Colt Steele's HTMLCSSJS Course/Functions/2 Arguments/app.js
@@ -0,0 +1,11 @@
+/*function greet(firstName, lastName){
+    console.log(`Hello, ${firstName} ${lastName[0]}.`)
+}
+*/
+
+function greet(firstName, lastName) {
+    console.log(`Hey there, ${firstName} ${lastName[0]}.`)
+}
+// copied the code from the lesson and had a brain fart, had to put args as in quotes or else js looks for variables and finds none. I'm retarded
+
+
