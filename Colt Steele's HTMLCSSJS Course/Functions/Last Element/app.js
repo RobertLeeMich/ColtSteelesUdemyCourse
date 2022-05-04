a16d703e39170f696dbe04562e8ac5742123ca18
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-04 11:20:00 -0400

    Update app.js
    added comments

--- Colt Steele's HTMLCSSJS Course/Functions/Last Element/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Last Element/app.js
@@ -7,3 +7,5 @@ function lastElement (arg){
 }
 
 //the order in which things are returned does actually matter. I had the arg[arg.length - 1] underneath the return null that was at the top of the function code.
+
+//3rd try + a lookup
