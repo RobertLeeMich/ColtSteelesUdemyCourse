9f2dd3f907f659cbb8dd1eebcb78baeea2d94fb6
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-04 08:58:51 -0400

    added new piece of lesson
    Added new piece of lesson for multiple args

--- Colt Steele's HTMLCSSJS Course/Functions/2 Arguments/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/2 Arguments/app.js
@@ -8,4 +8,14 @@ function greet(firstName, lastName) {
 }
 // copied the code from the lesson and had a brain fart, had to put args as in quotes or else js looks for variables and finds none. I'm retarded
 
+function repeat(str, numTimes) {
+    /*result starts as empty*/
+    let result = "";
+    /*we loop the number of times dictated in the argument */
+    for (let i = 0; i < numTimes; i++)
+    /*we assign the resulting string being looped to result and concatenate the looped strings together  */
+    result += str;
+    /*console.log the result */
+    console.log(result);
+}
 
