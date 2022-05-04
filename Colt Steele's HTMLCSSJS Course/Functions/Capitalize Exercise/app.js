a94ff21ed14f198a032a2d2fef4771959f3374ff
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-04 11:56:37 -0400

    capitalization quiz
    capitalization quiz with functions.

---
+++ Colt Steele's HTMLCSSJS Course/Functions/Capitalize Exercise/app.js
@@ -0,0 +1,11 @@
+/* function capitalize(arg) {
+    return arg.toUpperCase();
+} */
+
+//remember to put the () after calling a function so it runs. Herpaderpa.
+
+function capitalize (str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+//had to look up the solution to this since I didn't pay attention to instructions. 5 tries.
