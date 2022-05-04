65746f04276c527bff27066bf6ed98b8005e81a0
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-04 11:19:21 -0400

    Update app.js
    completed I think

--- Colt Steele's HTMLCSSJS Course/Functions/Last Element/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Last Element/app.js
@@ -1,7 +1,9 @@
 function lastElement (arg){
-    if (arg == []){
-        return null;
+    if (arg.length) {
+        return arg[arg.length - 1];
     } else {
-        return arg.slice() - 1;
+    return null;
     }
 }
+
+//the order in which things are returned does actually matter. I had the arg[arg.length - 1] underneath the return null that was at the top of the function code.
