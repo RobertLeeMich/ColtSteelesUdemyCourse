0eae0218094cd007a39560d362b3d4718efa9325
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-06 02:15:21 -0400

    day of the week retrieval quiz
    first iteration of using key/value pairs for getting the day of the week if a number is entered

---
+++ Colt Steele's HTMLCSSJS Course/Functions/Days of the Week Exercise/app.js
@@ -0,0 +1,23 @@
+function returnDay (num){
+    daysArray = {
+        1 : Monday,
+        2 : Tuesday,
+        3 : Wednesday,
+        4 : Thursday,
+        5 : Friday,
+        6 : Saturday,
+        7 : Sunday
+    }
+    if (num >= 7){
+        return null;
+    }
+    let dayValue = 
+    for (let value of Object.values(daysArray)){
+    }
+    console.log(value);
+    return dayValue;
+    
+    
+
+
+}
