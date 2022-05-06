83d6da503936341ec814db3277b35c230c035400
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-06 01:59:41 -0400

    Update app.js
    added descriptors of each line for learning purposes

--- Colt Steele's HTMLCSSJS Course/Functions/Sum Array Quiz/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Sum Array Quiz/app.js
@@ -1,9 +1,14 @@
+//creation of function
 function sumArray(array) {
+    //creation of total to hold sum of numbers from array
     let total = 0;
+    //creation of for loop to loop through array
     for (let i = 0; i < array.length; i++)
+    //assigning items in array to total
     {
         total += array[i]; 
     }
+    //return total to outside of array
     return total;
 }
 
