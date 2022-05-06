5afe4780c0fc8ef44c3af8bed47899b7caa61963
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-06 01:55:09 -0400

    Update app.js
    second iteration after trying to figure out why the code wasn't working generally. Notes in comments.

--- Colt Steele's HTMLCSSJS Course/Functions/Sum Array Quiz/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Sum Array Quiz/app.js
@@ -1,7 +1,10 @@
-let sum;
-function sumArray(arr) {
-    
-    for (let i = 0; i < arr.length; i++){
-        sum += arr[i]; 
+function sumArray(array) {
+    let total = 0;
+    for (let i = 0; i < array.length; i++)
+    {
+        total += array[i]; 
     }
+    return total;
 }
+
+//the total var can't be outside of the function on creation because we are assigning the variable a value inside the function and returning it outside the function. So we can use total OUTSIDE the function after the fact but we can't create it outside the function.
