677be9b8361a7aeebae611328f277733d78b3919
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-10 02:26:15 -0400

    block scoping
    already knew this but notes are notes

---
+++ Colt Steele's HTMLCSSJS Course/Functions/Scope/Block Scope/app.js
@@ -0,0 +1,11 @@
+let radius = 8;
+if (radius > 0){
+    const PI = 3.14159;
+    let msg = "HIIIII"
+}
+
+//"block" refers to curly braces used in code. If vars are declared within curly braces anywhere, it's only scoped in block code
+
+//if you use "var" for variable declaration though, it will be scoped outside of the curly braces. GENERALLY YOU DON'T EVER USE VAR ANYMORE
+
+//let and const are for following block-ed scoping rules, generally.
