485eecb340967d52ca28bf659cad38221d77779a
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-10 02:35:05 -0400

    lexicon scope (function in function)
    lesson for lexicon scope as defined in the lesson

---
+++ Colt Steele's HTMLCSSJS Course/Functions/Scope/Lexical Scope/app.js
@@ -0,0 +1,12 @@
+function bankRobbery() {
+    const heroes = ["Spiderman", "Wolverine", "Black Panther"]
+    function cryForHelp() {
+        for (let hero of heroes) {
+            console.log(`Please help ${hero.toUpperCase()}`)
+        }
+        
+    }
+    cryForHelp();
+}
+
+//Don't forget to add the () when calling a .[function] on something -_-
