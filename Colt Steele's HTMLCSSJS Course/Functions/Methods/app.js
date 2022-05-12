13410f5a8098857c7a2b147d3212f235b873f41e
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-12 01:48:50 -0400

    method
    made the method

--- Colt Steele's HTMLCSSJS Course/Functions/Methods/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Methods/app.js
@@ -2,3 +2,12 @@
 //[1,2,3].indexOf(0,2) <--- array method
 //etc
 
+const myMath = {
+    PI: 3.14159,
+    square: function (num){
+        return num * num;
+    },
+    cube: function (num) {
+        return num ** 3;
+    }
+}
