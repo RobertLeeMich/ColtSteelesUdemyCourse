46c05ba794a8489a381b5f937deec616d4ece726
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-12 01:42:10 -0400

    Update app.js
    he makes another example in the video but I think it's well explained here.

--- Colt Steele's HTMLCSSJS Course/Functions/Higher Order Functions/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Higher Order Functions/app.js
@@ -41,7 +41,7 @@ function makeBetweenFunc(min,max) {
 
 /* The example for using the above code goes like this:
 
-Assigning a the whole function to a variable, so we can do const isChild = makeBetweenFunc(0, 18) which when we call isChild(40) will return false and isChild(5) will return true. Then we can assign the same function to another variable called isAdult (const isAdult) and assign the "factory" to it again with the limits of 19 - 100 as an example (const isAdult = makeBetweenFunc(19, 100)). Then we can call isAdult(40) which would return true and isAdult(5) which is false.
+Assigning a the whole function to a variable, so we can do const isChild = makeBetweenFunc(0, 18) which when we call isChild(40) will return false and isChild(5) will return true. Then we can assign the same function to another variable called isAdult (const isAdult) and assign the "factory" to it again with the limits of 19 - 100 as an example (const isAdult = makeBetweenFunc(19, 100)). Then we can call isAdult(40) which would return true and isAdult(5) which is false, etc., etc.
 
 */
 
