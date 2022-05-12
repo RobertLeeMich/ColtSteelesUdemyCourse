341b69686be8e9cf582a3bb3a4c88d81abdb3347
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-12 01:58:49 -0400

    Update app.js
    "square" object asks for arrow notation to declare it a variable for it to work. Not sure why.

--- Colt Steele's HTMLCSSJS Course/Functions/Methods/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Methods/app.js
@@ -2,12 +2,25 @@
 //[1,2,3].indexOf(0,2) <--- array method
 //etc
 
-const myMath = {
-    PI: 3.14159,
-    square: function (num){
-        return num * num;
+// const myMath = {
+//     PI: 3.14159,
+//     square: function (num){
+//         return num * num;
+//     },
+//     cube: function (num) {
+//         return num ** 3;
+//     }
+// }
+
+//The above can be rewritten as:
+
+const myMath2 = {
+    PI : 3.14159,
+    square: (num2) => {
+        return num2 * num2;
     },
-    cube: function (num) {
-        return num ** 3;
+    cube (num2) {
+        return num2 ** 3
     }
 }
+
