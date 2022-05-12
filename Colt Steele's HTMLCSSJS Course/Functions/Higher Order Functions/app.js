3557c35723160b1319e111ec4ebf5ff9549e81b5
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-12 01:27:54 -0400

    Update app.js
    made example higher order function

--- Colt Steele's HTMLCSSJS Course/Functions/Higher Order Functions/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Higher Order Functions/app.js
@@ -15,3 +15,18 @@ function rollDie(){
     const roll = Math.floor(Math.random() * 6) + 1
     console.log(roll)
 }
+
+//Returning Functions lesson in Higher Order Functions lesson
+
+function makeMysteryFunc(){
+    const rand = math.Random();
+    if (rand > 0.5) {
+        return function() {
+            console.log("Congrats, good function")
+        }
+    } else {
+        return function() {
+            console.log("Bad function wheewhoo")
+        }
+    }
+}
