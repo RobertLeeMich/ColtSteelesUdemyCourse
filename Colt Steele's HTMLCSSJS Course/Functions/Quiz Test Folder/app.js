72d3979a59efb938a382aa422eed3fc9869d37a8
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-13 13:12:55 -0400

    Update app.js
    updated with notes

--- Colt Steele's HTMLCSSJS Course/Functions/Quiz Test Folder/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Quiz Test Folder/app.js
@@ -6,3 +6,5 @@ const hen = {
         console.log("EGG");
     }
 }
+
+//had to look up help, turns out it was easier than my brain was making it out to be. Just directly reference this.eggcount to increment the count instead of using a loop since we're just trying to add 1 to each call and not a bunch all at once.
