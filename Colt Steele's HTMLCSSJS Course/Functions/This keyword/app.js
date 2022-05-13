ef43af4cca0dabe8a8ebb9340f1ef049d50801ff
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-13 12:50:42 -0400

    Update app.js
    next part of lesson, assigning the object to a variable and then calling it to view the example of the fact the .this isn't referenced in the function/variable call

--- Colt Steele's HTMLCSSJS Course/Functions/This keyword/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/This keyword/app.js
@@ -8,3 +8,9 @@ const cat = {
 }
 
 //inside of the object, the keyword .this refers to the object itself, in this case, cat
+
+//But, the value of .this depends on the invocation context of the function it is used in
+
+const meow2 = cat.meow;
+
+//when we call the above line of code with meow2(), it just prints "says meow". 
