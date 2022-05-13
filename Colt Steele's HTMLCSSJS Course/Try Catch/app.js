cff419c6a69b8fb4953b8ad8c831716e113c8879
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-13 13:21:15 -0400

    try/catch
    try/catch example

---
+++ Colt Steele's HTMLCSSJS Course/Try Catch/app.js
@@ -0,0 +1,14 @@
+//try catch is to catch errors so they don't absolutely decimate the running of code. If we think something will result in an error, wrap it in a try/catch.
+
+try {
+    hello.toUpperCase
+} catch {
+    console.log("error")
+}
+
+console.log("after")
+//the above right off the bat shows the console.log in the chrome console before trying to run any code
+
+//the console.log below it will still run even though the hello.toUpperCase still errors out
+
+//using try/catch is important in AJAX etc. 
