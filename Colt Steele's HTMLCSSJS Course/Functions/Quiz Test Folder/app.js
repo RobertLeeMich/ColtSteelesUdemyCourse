cdcbe8201fb79da15acc30711686ca5a9e9b3e16
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-13 13:15:55 -0400

    Update app.js
    had to return the value, duh

--- Colt Steele's HTMLCSSJS Course/Functions/Quiz Test Folder/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Quiz Test Folder/app.js
@@ -9,8 +9,10 @@ const hen = {
         //this here refers to "hen", .eggCount refers to property in object that is numberical
         this.eggCount += 1
         //logs to console
-        console.log("EGG");
+        return ("EGG");
     }
 }
 
 //had to look up help, turns out it was easier than my brain was making it out to be. Just directly reference this.eggcount to increment the count instead of using a loop since we're just trying to add 1 to each call and not a bunch all at once.
+
+//RETURN IN DESCRIPTION MEANS RETURN!
