64825cc4611378f006f72edbe1efbeb60fe6b531
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-16 22:27:47 -0400

    Update app.js
    for posterity I just put the wrong code I put down first in here for the .map lesson

--- Colt Steele's HTMLCSSJS Course/Functions/Quiz Test Folder/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Quiz Test Folder/app.js
@@ -1,18 +1,7 @@
-//declare object
-const hen = {
-    //declare object property
-    name: "Helen",
-    //declare object property
-    eggCount: 0,
-    //declare function in object that changes value of another property
-    layAnEgg() {
-        //this here refers to "hen", .eggCount refers to property in object that is numberical
-        this.eggCount += 1
-        //logs to console
-        return ("EGG");
-    }
-}
+// DO NOT ALTER THE FOLLOWING CODE:
+const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
 
-//had to look up help, turns out it was easier than my brain was making it out to be. Just directly reference this.eggcount to increment the count instead of using a loop since we're just trying to add 1 to each call and not a bunch all at once.
-
-//RETURN IN DESCRIPTION MEANS RETURN!
+// Write your code here
+const firstNames = firstNames.map(function(names){
+    return fullNames.firstNames;
+})
