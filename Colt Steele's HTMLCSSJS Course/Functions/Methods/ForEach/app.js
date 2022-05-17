b2832eb146237f222ddcaeb2185b64138953fd72
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-16 22:04:33 -0400

    forEach
    two pieces of code that do the same thing

--- Colt Steele's HTMLCSSJS Course/Functions/Methods/ForEach/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Methods/ForEach/app.js
@@ -1,1 +1,14 @@
 const nums = [1,2,3,4,5,6,7,8,9,10]
+
+//forEach prints each item in an array
+//inline function declaration to print each item rather than declaring a print function and calling the function in the forEach arg
+
+
+
+nums.forEach(function(el){
+    console.log(el);
+})
+
+for (let el of nums) {
+    console.log(el);
+}
