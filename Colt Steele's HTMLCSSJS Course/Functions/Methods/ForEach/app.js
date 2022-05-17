0b1b5fad9c755f54353b095ecd0038da22d5e0b0
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-16 22:06:39 -0400

    Update app.js
    another example of forEach

--- Colt Steele's HTMLCSSJS Course/Functions/Methods/ForEach/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Methods/ForEach/app.js
@@ -3,12 +3,19 @@ const nums = [1,2,3,4,5,6,7,8,9,10]
 //forEach prints each item in an array
 //inline function declaration to print each item rather than declaring a print function and calling the function in the forEach arg
 
+//modulo operator (divisible by x number w/o remainder) printed to console, so 2, 4, 6, etc.
 
-
-nums.forEach(function(el){
-    console.log(el);
+nums.forEach(function (el){
+    if (el % 2 === 0) {
+console.log(el)
+    }
 })
 
-for (let el of nums) {
-    console.log(el);
-}
+
+// nums.forEach(function(el){
+//     console.log(el);
+// })
+
+// for (let el of nums) {
+//     console.log(el);
+// }
