bb90ce3ea0e33d835528b8d5dcbf246937eb6962
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-16 22:32:13 -0400

    Update app.js
    mistakes have been had

--- Colt Steele's HTMLCSSJS Course/Functions/Quiz Test Folder/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Quiz Test Folder/app.js
@@ -2,6 +2,6 @@
 const fullNames = [{first: 'Albus', last: 'Dumbledore'}, {first: 'Harry', last: 'Potter'}, {first: 'Hermione', last: 'Granger'}, {first: 'Ron', last: 'Weasley'}, {first: 'Rubeus', last: 'Hagrid'}, {first: 'Minerva', last: 'McGonagall'}, {first: 'Severus', last: 'Snape'}];
 
 // Write your code here
-const firstNames = firstNames.map(function(names){
-    return fullNames.firstNames;
+const firstNames = fullNames.map(function(name){
+    return name.firstNames;
 })
