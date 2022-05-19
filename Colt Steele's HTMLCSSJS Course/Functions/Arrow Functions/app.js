457018e869298d8fe4bcfd510d04b6ab92c66b64
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-19 19:22:56 -0400

    Update app.js
    different arrow functions

--- Colt Steele's HTMLCSSJS Course/Functions/Arrow Functions/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Arrow Functions/app.js
@@ -2,6 +2,22 @@
 //     console.log("ooooooh")
 // }
 
-const test2 = (arg) => {
-    console.log("oooooh")
+// const test2 = (arg) => {
+//     console.log("oooooh")
+// }
+
+// const add = function(x,y){
+//     return x+y;
+// }
+
+const add = (x,y) => {
+    return x + y;
+}
+//can write without parenthesis if you are only using 1 argument
+const sq = x => {
+    return x * x
+}
+//have to have empty parenthesis if no args
+const rollDie = () => {
+    return Math.floor(Math.random() * 6) +1
 }
