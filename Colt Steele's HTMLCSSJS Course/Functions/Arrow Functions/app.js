41048536c6c567a725bcd6e63fe4c4777e254d84
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-19 19:33:42 -0400

    Update app.js
    Implicit returns using implicit arrow functions

--- Colt Steele's HTMLCSSJS Course/Functions/Arrow Functions/app.js
+++ Colt Steele's HTMLCSSJS Course/Functions/Arrow Functions/app.js
@@ -18,6 +18,16 @@ const sq = x => {
     return x * x
 }
 //have to have empty parenthesis if no args
-const rollDie = () => {
-    return Math.floor(Math.random() * 6) +1
-}
+// const rollDie = () => {
+//     return Math.floor(Math.random() * 6) +1
+// }
+
+//implicit arrow functions
+//replacing the brackets with parenthesis works with "returns" that return ONE value only
+const rollDie = () => (
+    Math.floor(Math.random() * 6) +1
+)
+
+//this is the shortest implicit arrow function that can be done that is functional, generally. One line.
+const add2 = (a, b) => a + b
+
