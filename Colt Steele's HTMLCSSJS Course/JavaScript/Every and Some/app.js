cc7be16d2658893b7ccc80bf55cc3780cf7b2ab1
Robert Michaud <guildwarsguy123@yahoo.com>
2022-05-29 01:01:26 -0400

    some/every
    some and every lesson. Fairly straightforward

---
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Every and Some/app.js
@@ -0,0 +1,10 @@
+const exams = [ 90, 92, 94, 96, 75, 43, 20, 1, 10, 74]
+
+exams.every(score => score >= 75)
+//returns false
+
+exams.some(score => score >= 75)
+//true bc "some" of the items in the list are above 75, it just doesn't specify which
+
+//the lesson uses the movies object array again but I'm gonna skip this. I get the point
+
