dc78e3fe215aab5a28eaf98e7e484ca7346871c2
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-02 12:55:28 -0400

    Update app.js
    completed arrow notation and this lesson with explanation.

--- Colt Steele's HTMLCSSJS Course/JavaScript/Functions/Arrow Functions/Arrow Functions and this/app.js
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Functions/Arrow Functions/Arrow Functions and this/app.js
@@ -6,5 +6,13 @@ const person = {
     },
     //running fullName in a console will return Robert Michaud
     //also remember to have the code actually run you have to put person.fullName() with parenthesis. 
+    //if you replace the above with arrow notation, the 'this' in the code references the window instead of the object.
+    shoutName: function () {
+        setTimeout(() => {
+            console.log(this);
+            console.log(this.fullName())
+        }, 3000)
+    }
+    //the above code is a function in a function that references the .fullName reference after 3 seconds. If the fullName key has an arrow function in it though, the console will print "undefined undefined" because it would be pointing to the window object.
 }
 
