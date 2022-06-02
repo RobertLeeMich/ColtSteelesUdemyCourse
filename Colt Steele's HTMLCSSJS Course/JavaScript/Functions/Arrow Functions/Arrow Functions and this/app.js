da1ff461b1bf30e4a6c125f1b54791db97545e4a
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-02 12:42:38 -0400

    arrow functions and this
    gotta remember the parenthesis

--- Colt Steele's HTMLCSSJS Course/JavaScript/Functions/Arrow Functions/Arrow Functions and this/app.js
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Functions/Arrow Functions/Arrow Functions and this/app.js
@@ -1,4 +1,10 @@
 const person = {
-    firstName: 'Robert',
-    lastName: 'Michaud'
+    firstName: 'Bob',
+    lastName: 'Mich',
+    fullName: function () {
+        return `${this.firstName} ${this.lastName}`
+    },
+    //running fullName in a console will return Robert Michaud
+    //also remember to have the code actually run you have to put person.fullName() with parenthesis. 
 }
+
