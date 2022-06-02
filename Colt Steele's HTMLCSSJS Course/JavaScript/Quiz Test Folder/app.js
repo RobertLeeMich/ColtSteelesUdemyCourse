d9409a370af952dd15df280c7b024112c73079ae
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-02 11:54:18 -0400

    Update app.js
    I should have just referenced this. The "num" is "score" here which is just the stand in for the numbers being passed in to check against the condition.

--- Colt Steele's HTMLCSSJS Course/JavaScript/Quiz Test Folder/app.js
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Quiz Test Folder/app.js
@@ -2,3 +2,12 @@ function allEvens(arr) {
     return arr.every(num => num % 2 === 0)
 }
 
+const exams = [ 90, 92, 94, 96, 75, 43, 20, 1, 10, 74]
+
+exams.every(score => score >= 75)
+//returns false
+
+exams.some(score => score >= 75)
+//true bc "some" of the items in the list are above 75, it just doesn't specify which
+
+//the lesson uses the movies object array again but I'm gonna skip this. I get the point
