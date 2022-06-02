9a50921496f34ea65f432ab8e116910206e6c1a3
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-02 12:05:08 -0400

    initial lesson
    initial lesson code before shortening

--- Colt Steele's HTMLCSSJS Course/JavaScript/Reduce/app.js
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Reduce/app.js
@@ -2,3 +2,13 @@ const arr = [9.99, 1.50, 19.99, 49.99, 30.50];
 
 let total = 0;
 
+//example reduce code:
+//[3, 5, 7, 9, 11].reduce((accumulator, currentValue) => {
+//return accumulator + currentValue
+//})
+//In the above code, the reduce function works by taking the accumulator which in this instance is 3 and currentValue points to 5. This makes 8. So now 8 is the accumulator and 7 is the currentValue which makes 15. Then 15 is the accumulator and 9 is the currentValue, etc etc to the ending value of 35.
+
+total = arr.reduce((total, price) => {
+    return total + price
+})
+//total comes to 111.97 which is correct.
