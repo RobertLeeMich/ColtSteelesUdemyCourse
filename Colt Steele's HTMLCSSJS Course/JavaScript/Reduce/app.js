ed0f5fa89016834222d1261c1028356fa8588f77
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-02 12:13:20 -0400

    Update app.js
    code explanation for comparing 2 prices to get the smallest price in the list.

--- Colt Steele's HTMLCSSJS Course/JavaScript/Reduce/app.js
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Reduce/app.js
@@ -15,3 +15,16 @@ const arr = [9.99, 1.50, 19.99, 49.99, 30.50];
 //This can, of course be shortened further:
 
 const total = arr.reduce((total, price) => total + price)
+
+//you can do any mathematical operation here. But you can do more with reduce.
+
+//minimum price:
+//declaration of var holding the reduce function on arr. min here is the minimum price that will be compared to the value (currentPrice) that is being looked at to see if it is smaller than min or not
+const minPrice = arr.reduce((min, currentPrice) => {
+//This says if currentPrice (the number being looked at) is smaller than min, we return currentPrice out of the code. Or else we return min out of the code if currentPrice is LARGER than min
+    if (currentPrice < min){
+        return currentPrice;
+    }
+    return min;
+})
+//this code returns to console 1.5 which is the smallest price.
