57d58b8d7b261f05b2a5144ecaf6a53a3d3eee0f
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-08 23:03:02 -0400

    Update app.js
    old way of doing default parameters

--- Colt Steele's HTMLCSSJS Course/JavaScript/Default Params/app.js
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Default Params/app.js
@@ -1,7 +1,15 @@
-function rollDie (numSides){
+function rollDie(numSides){
     return Math.floor(Math.random() * numSides) +1
 }
 
 //default numSides to 6?
 //if we don't pass in a number to the arg, we get NaN
 
+function rollDie2(numSides){
+    if (numSides === undefined){
+        numSides = 6
+    }
+    return Math.floor(Math.random() * numSides) +1
+}
+
+//above is the old way of doing default parameters which makes sense, if the number comes out to be NaN/undefined, then it's 6.
