74fb5af227a8c77938084e68f128f0c6a27a84b0
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-20 01:07:27 -0400

    Update app.js
    destructuring arrays completed

--- Colt Steele's HTMLCSSJS Course/JavaScript/Destructuring/app.js
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Destructuring/app.js
@@ -1,5 +1,15 @@
 //destructuring is a way to single out values from arrays or objects
 
-const scores = [9932, 23941, 02932, 58493, 22931, 65837, 41954, 62938, 75869]
+const scores = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 //today I learned that array items can't begin with 0 or numbers in general in JS because JS views it as base 8 or 'oct' type numbers. thanks Sreject
+
+const highScore = scores[0];
+const secondHighScore = scores[1];
+
+const [gold, silver, bronze, ...everyoneElse] = scores;
+
+//calling gold prints 10
+//calling silver prints 9
+//calling bronze prints 8
+//calling everyoneElse prints 7,6,5 etc.
