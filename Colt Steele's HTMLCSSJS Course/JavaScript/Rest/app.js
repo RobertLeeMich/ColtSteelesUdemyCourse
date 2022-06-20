8a7bf011f5e143f3daf31d6784caa6208b7bd546
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-20 00:22:16 -0400

    Update app.js
    added use of rest

--- Colt Steele's HTMLCSSJS Course/JavaScript/Rest/app.js
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Rest/app.js
@@ -1,8 +1,14 @@
 //rest looks similar to spread but is different
 
-function sum(){
-    console.log(arguments)
-    return arguments.reduce((total, el) => total + el)
-}
+//rest collects all remaining arguments into an array
+
+// function sum(){
+//     return arguments.reduce((total, el) => total + el) //this portion was not explained well and I'm just leaving it here for notes
+// }
 //doing this lets you analyze args in the output
+
+function sum (...nums){
+    console.log(nums)
+}
+//the (...) here is actually a rest, which will pull all associated numbers in the arguments into the argument from whatever the user puts in for args
 
