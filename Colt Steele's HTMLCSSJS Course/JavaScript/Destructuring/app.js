6f9a31c1d052618897f7ced56caa210d7eb84c6c
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-20 00:54:06 -0400

    TIL...
    TIL base 8 numbers after trying to make my array numbers start with 0

---
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Destructuring/app.js
@@ -0,0 +1,5 @@
+//destructuring is a way to single out values from arrays or objects
+
+const scores = [9932, 23941, 02932, 58493, 22931, 65837, 41954, 62938, 75869]
+
+//today I learned that array items can't begin with 0 or numbers in general in JS because JS views it as base 8 or 'oct' type numbers. thanks Sreject
