0e82e8da182e174c7185c06cc6e8cec5fdba69f0
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-28 09:23:30 -0400

    Update app.js
    end of lesson.

--- Colt Steele's HTMLCSSJS Course/JavaScript/Destructuring/Destructuring Objects/app.js
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Destructuring/Destructuring Objects/app.js
@@ -16,7 +16,6 @@ const user2 = {
     firstName: "User2",
     lastName: "Name2",
     born: 1999,
-    died: 2004,
     bio: "Username2 is a username2",
     city: "Tulsa",
     state: "Oklahoma"
@@ -24,10 +23,12 @@ const user2 = {
 
 //const email = user.email;
 //The above is the same as below.
-const {email, firstName, lastName, city, bio} = user;
+// const {email, firstName, lastName, city, bio} = user;
 
 //If we want to rename a property (ex: for use somewhere else but with a different name for example) we just do the below
-const { born: birthYear, died: deathYear } = user;
+// const { born: birthYear, died: deathYear } = user;
 
+const {city, state, died = 2004} = user2;
+//default/fallback values can be assigned via = 
 
 
