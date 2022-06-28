406b43cd7f1e82c07ace355ccd2c9fddd2e55e90
Robert Michaud <guildwarsguy123@yahoo.com>
2022-06-28 09:01:26 -0400

    destructuring objects

---
+++ Colt Steele's HTMLCSSJS Course/JavaScript/Destructuring/Destructuring Objects/app.js
@@ -0,0 +1,11 @@
+const user = {
+    email: "user@gmail.com",
+    password: "password",
+    firstName: "User",
+    lastName: "Name",
+    born: 1977,
+    died: 1978,
+    bio: "Username is a username",
+    city: "Los Angeles",
+    state: "Utah"
+}
